7d5a530cd2b82bd32255762f4f27d2cb54aaefdc
Bik097 <bjs241997@gmail.com>
2019-09-25 15:53:49 -0400

    Add project files.

---
+++ Lab2/routes/Lab2.js
@@ -0,0 +1,44 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+
+    var Xresult = parseInt(req.query.x);
+    var Yresult = parseInt(req.query.y);
+
+    //res.render('index', { title: 'Express' });
+    if (req.query.method == 'add' && Xresult && Yresult) {
+
+
+        res.send(Xresult + " + " + Yresult + " = " + ((Xresult + Yresult)).toString());
+    }
+
+    if (req.query.method == 'subtract' && Xresult && Xresult) {
+
+
+        res.send(Xresult + " - " + Yresult + " = " + ((Xresult - Yresult)).toString());
+    }
+
+    if (req.query.method == 'multiply' && Xresult && Xresult) {
+
+
+        res.send(Xresult + " * " + Yresult + " = " + ((Xresult * Yresult)).toString());
+    }
+
+    if (req.query.method == 'divide' && Xresult && Xresult) {
+
+
+        res.send(Xresult + " / " + Yresult + " = " + ((Xresult / Yresult)).toString());
+    }
+
+
+
+    else {
+
+        res.send("Incorrect Input");
+    }
+});
+
+module.exports = router;
